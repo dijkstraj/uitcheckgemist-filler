d1f67e053d53fe572887ddefa4605bf0de29a684
Jogchem Dijkstra <jogchem@gmail.com>
2017-03-14 22:55:48 +0100

    Overlay

--- filler.js
+++ filler.js
@@ -1,34 +1,41 @@
 if (document.getElementById('tls_card_information_expirationDate')) {
   chrome.runtime.sendMessage({method: "getDetails"}, function(response) {
     let details = response.data;
-    details.forEach((detail) => {
-      let node = document.createElement('button');
-      let textNode = document.createTextNode(detail.name);
-      node.appendChild(textNode);
-      document.getElementsByTagName('h3')[0].appendChild(node);
-      node.addEventListener('click', (event) => {
-        event.preventDefault();
+    if (details.length > 0) {
+      let overlay = document.createElement('div');
+      overlay.id = 'filler-overlay';
+      let container = document.createElement('div');
+      details.forEach((detail) => {
+        let node = document.createElement('button');
+        let textNode = document.createTextNode(detail.name);
+        node.appendChild(textNode);
+        container.appendChild(node);
+        node.addEventListener('click', (event) => {
+          event.preventDefault();
 
-        // enter card number
-        detail.number.forEach((part, index) => {
-          if (index > 0) {
-            document.getElementById('tls_card_information_engravedId_' + index).value = detail.number[index];
-          }
-        });
+          // enter card number
+          detail.number.forEach((part, index) => {
+            if (index > 0) {
+              document.getElementById('tls_card_information_engravedId_' + index).value = detail.number[index];
+            }
+          });
 
-        // enter expiry date
-        document.getElementById('tls_card_information_expirationDate').value = detail.valid;
+          // enter expiry date
+          document.getElementById('tls_card_information_expirationDate').value = detail.valid;
 
-        // check the box
-        document.getElementById('tls_card_information_optIn').click();
+          // check the box
+          document.getElementById('tls_card_information_optIn').click();
 
-        // store date of birth for next screen
-        window.localStorage.birthdate = detail.birthdate;
+          // store date of birth for next screen
+          window.localStorage.birthdate = detail.birthdate;
 
-        // go to next page
-        document.getElementsByClassName('submit')[0].click();
-      });  
-    });
+          // go to next page
+          document.getElementsByClassName('submit')[0].click();
+        });  
+      });
+      overlay.appendChild(container);
+      document.body.appendChild(overlay);
+    }
   });
 } else if (document.getElementById('tls_person_information_holderBirthDate')) {
   // retrieve date of birth
